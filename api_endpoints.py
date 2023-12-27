6f95105e424b9006aeb2c80e7ebe723b94244da5
blaze <Arnnav0kudale@gmail.com>
2023-12-27 15:52:34 -0500

    most of the backend is finished

--- api_endpoints.py
+++ api_endpoints.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, render_template
+import ctypes
+import time
+from flask import Flask, jsonify
 import psutil
 import threading
-import ctypes
 
 app = Flask(__name__)
 
@@ -15,14 +16,24 @@ def unlock_game_content():
     return "Game content unlocked for all users"
 
 def lock_game_content():
-    exit_fortnite()
+    exit_slack()
     global game_locked
     game_locked = True
-    show_popup_message("To play games, complete your work!")
-    return jsonify({'message': 'Game content locked for all users'})
+    show_yes_no_popup("Have you completed your work?", "Slack")
+    return jsonify({'message': 'Slack content locked for all users'})
 
-def show_popup_message(message):
-    ctypes.windll.user32.MessageBoxW(0, message, "Message", 1)
+def show_yes_no_popup(message, title):
+    result = ctypes.windll.user32.MessageBoxW(0, message, title, 1 | 0x40)  # MB_YESNO flag
+
+    if result == 1:  # User clicked "Yes"
+        # do thingy here
+        print("Yes")
+    else:
+        # Terminate the Slack application or perform other actions
+        for process in psutil.process_iter(['pid', 'name']):
+            if process.info['name'] == 'slack.exe':
+                psutil.Process(process.info['pid']).terminate()
+
 
 def run_game():
     # Check if the game is unlocked
@@ -31,18 +42,24 @@ def run_game():
     else:
         return "Game is locked. Complete the task to unlock."
 
-def exit_fortnite():
+def exit_slack():
     for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'] == 'FortniteClient-Win64-Shipping.exe':
-            print("Terminating...")
-            psutil.Process(process.info['pid']).terminate()
-            print("Terminated.")
-            show_popup_message("To play games, complete your work!")
+        if process.info['name'] == 'slack.exe':
+            print("Terminating Notepad...")
+            try:
+                show_yes_no_popup("Have you completed your work?", "Slack")
+
+                # psutil.Process(process.info['pid']).terminate()
+                print("Notepad terminated.")
+            except psutil.NoSuchProcess:
+                print("Notepad process not found.")
             return
 
-def check_and_exit_fortnite():
-    if game_locked:
-        exit_fortnite()
+def check_and_exit_notepad():
+    while True:
+        if game_locked:
+            exit_slack()
+        time.sleep(1)
 
 @app.route('/unlock', methods=['GET', 'POST'])
 def unlock():
@@ -50,17 +67,18 @@ def unlock():
 
 @app.route('/lock', methods=['GET', 'POST'])
 def lock():
-    exit_fortnite()
+    exit_slack()
     return lock_game_content()
 
-@app.route('/exit', methods=['POST', 'GET'])
-def exit_fortnite_endpoint():
-    exit_fortnite()
-    return jsonify({'message': 'Exiting Fortnite'})
+@app.route('/run', methods=['GET', 'POST'])
+def run():
+    return jsonify({'message': run_game()})
+
+@app.route('/assignment_data', methods=['POST'])
+def thatscrayz():
+    return jsonify({"hi": "ur mom heheheha"})
 
 if __name__ == '__main__':
-    # Start the Flask application in the main thread
+    # Start the Flask application in a separate thread
+    threading.Thread(target=check_and_exit_notepad, daemon=True).start()
     app.run(debug=True, use_reloader=False)
-
-    # Start the thread to check and exit Fortnite
-    threading.Thread(target=check_and_exit_fortnite).start()
